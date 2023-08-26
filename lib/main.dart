2ee7ab8302b7e890a4621e6eab326129c2e705bc
taizen115 <taizen115@gmail.com>
2023-08-26 12:41:27 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:citrus_list/style/style.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CitrusList",
+      theme: ThemeData(
+        brightness: Brightness.light,
+        fontFamily: MainFont
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
