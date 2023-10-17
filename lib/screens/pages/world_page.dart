91c7d5f1a450a482887f2b9227ca10d4f42ac5be
taizen115 <taizen115@gmail.com>
2023-10-17 17:02:33 +0900

    2023.10.17

--- lib/screens/pages/world_page.dart
+++ lib/screens/pages/world_page.dart
@@ -5,7 +5,7 @@ class WorldPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        //TODO
+        //TODO　日本全国の柑橘を集めよう。
         body: Column(
           children: [
             Expanded(child: Image.asset("assets/images/world_citrus.jpg",fit: BoxFit.fitWidth),),
